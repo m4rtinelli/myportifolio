112d4668d621e47ff219e6568d3f051856dc8a6b
Martinelli303 <martinelli303@hotmail.com>
2025-03-23 11:52:46 -0300

    build with different pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
-import restart from 'vite-plugin-restart'
+import { resolve } from "path";
+import restart from "vite-plugin-restart";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+  root: "src/", // Sources files (typically where index.html is)
+  publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        motiondesign: resolve(root, "motiondesign", "index.html"),
+      },
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
-}
+  },
+  plugins: [
+    restart({ restart: ["../static/**"] }), // Restart server on static file change
+  ],
+};
