1e1c9e5b6a1ded41872b11dff31feb6f4df3f3d7
m4rtinelli <martinelli303@hotmail.com>
2025-08-29 11:44:55 -0300

    english text fix and home welcome text fix

--- src/modules/textIntro.js
+++ src/modules/textIntro.js
@@ -1,22 +1,22 @@
-const welcomeTexts = [
-  "Bem-vindo!",
-  "Welcome!",
-  "¡Bienvenido!",
-  "Bienvenue!",
-  "Willkommen!",
-  "Benvenuto!",
-  "ようこそ!",
-  "欢迎!",
-];
+// const welcomeTexts = [
+//   "Bem-vindo!",
+//   "Welcome!",
+//   "¡Bienvenido!",
+//   "Bienvenue!",
+//   "Willkommen!",
+//   "Benvenuto!",
+//   "ようこそ!",
+//   "欢迎!",
+// ];
 
-let index = 0;
-const welcomeElement = document.getElementById("welcomeText");
-if (welcomeElement) {
-  setInterval(() => {
-    index = (index + 1) % welcomeTexts.length;
-    welcomeElement.textContent = welcomeTexts[index];
-  }, 3000);
-}
+// let index = 0;
+// const welcomeElement = document.getElementById("welcomeText");
+// if (welcomeElement) {
+//   setInterval(() => {
+//     index = (index + 1) % welcomeTexts.length;
+//     welcomeElement.textContent = welcomeTexts[index];
+//   }, 3000);
+// }
 
 const sections = {
   homeBt: document.querySelector(".hero-logo"),
