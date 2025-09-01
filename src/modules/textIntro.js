463e2de204333bdd13b54de7eac35aa45e00a228
m4rtinelli <martinelli303@hotmail.com>
2025-09-01 13:13:09 -0300

    fixing info txt sizing on smaller screens, removed scroll behaviour on gsap

--- src/modules/textIntro.js
+++ src/modules/textIntro.js
@@ -1,23 +1,3 @@
-// const welcomeTexts = [
-//   "Bem-vindo!",
-//   "Welcome!",
-//   "¡Bienvenido!",
-//   "Bienvenue!",
-//   "Willkommen!",
-//   "Benvenuto!",
-//   "ようこそ!",
-//   "欢迎!",
-// ];
-
-// let index = 0;
-// const welcomeElement = document.getElementById("welcomeText");
-// if (welcomeElement) {
-//   setInterval(() => {
-//     index = (index + 1) % welcomeTexts.length;
-//     welcomeElement.textContent = welcomeTexts[index];
-//   }, 3000);
-// }
-
 const sections = {
   homeBt: document.querySelector(".hero-logo"),
   contactBt: document.querySelector(".contact"),
